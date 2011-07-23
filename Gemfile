1ce4a53319c9942705369bdc5db15bc920d62cde
Steve Sloan <steve@finagle.org>
2011-07-23 16:09:57 -0700

    Update layout, style, added Compass.

--- Gemfile
+++ Gemfile
@@ -3,6 +3,8 @@ source :gemcutter
 gem 'bundler'
 gem 'sinatra'
 gem 'activesupport', :require => 'active_support'
+
+gem 'compass'
 gem 'haml'
 gem 'RedCloth'
 gem 'sass'
--- index.rb
+++ index.rb
@@ -1,3 +1,12 @@
+configure do
+  Compass.configuration do |config|
+    config.project_path = File.dirname(__FILE__)
+    config.sass_dir = 'views'
+  end
+
+  set :haml, { :format => :html5 }
+  set :sass, Compass.sass_engine_options
+end
 set :root, File.dirname(__FILE__)
 
 get '/' do
